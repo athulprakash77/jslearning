c0cd7fc90138fc6b60369d61bc04f2296bec37fc
athulprakash007 <athull362@gmail.com>
2022-05-28 11:46:50 -0700

    add new file small

---
+++ large.js
@@ -0,0 +1,10 @@
+var x=100
+var y=50
+if(x>y)
+{
+    console.log(x)
+}
+else
+{
+console.log(y)
+}
---
+++ small.js
@@ -0,0 +1,10 @@
+var x=50
+var y=110
+if(x<y)
+{
+    console.log(x)
+}
+else
+{
+console.log(y)
+}
